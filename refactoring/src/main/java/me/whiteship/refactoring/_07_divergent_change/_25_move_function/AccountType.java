50cb5287a58e09a1cf449ed86cd48e347921f1c3
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-12-08 07:40:36 +0900

    feat : refactoring divergent_change, shotgun surgery

--- refactoring/src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
@@ -10,4 +10,19 @@ public class AccountType {
     public boolean isPremium() {
         return this.premium;
     }
+
+    // overdraftCharge에서 Account를 넘기는 것이 맞는가? 강의는 데이터가 많을 경우 Account를 넘기는 것이 타당하다고 보나 (책)
+    // 이번 경우에는 int daysOverdrawn 값만 넘기는 것이 타당하다고 선택.
+    public double overdraftCharge(int daysOverdrawn) {
+        if (this.isPremium()) {
+            final int baseCharge = 10;
+            if (daysOverdrawn <= 7) {
+                return baseCharge;
+            } else {
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+            }
+        } else {
+            return daysOverdrawn * 1.75;
+        }
+    }
 }
--- refactoring/src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
@@ -2,21 +2,50 @@ package me.whiteship.refactoring._08_shotgun_surgery._29_inline_class;
 
 public class Shipment {
 
-    private TrackingInformation trackingInformation;
+//    private TrackingInformation trackingInformation;
 
-    public Shipment(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
+    // 1. 필드 이동
+    private String shippingCompany;
+
+    private String trackingNumber;
+
+//    public Shipment(TrackingInformation trackingInformation) {
+//        this.trackingInformation = trackingInformation;
+//    }
+
+    // 2. 필드에 해당하는 생성자 생성
+    public Shipment(String shippingCompany, String trackingNumber) {
+        this.shippingCompany = shippingCompany;
+        this.trackingNumber = trackingNumber;
     }
 
-    public TrackingInformation getTrackingInformation() {
-        return trackingInformation;
-    }
-
-    public void setTrackingInformation(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
-    }
+//    public TrackingInformation getTrackingInformation() {
+//        return trackingInformation;
+//    }
+//
+//    public void setTrackingInformation(TrackingInformation trackingInformation) {
+//        this.trackingInformation = trackingInformation;
+//    }
 
     public String getTrackingInfo() {
-        return this.trackingInformation.display();
+        return this.shippingCompany + ": " + this.trackingNumber;
     }
+
+    // 3. 메서드 이동
+    public String getShippingCompany() {
+        return shippingCompany;
+    }
+
+    public void setShippingCompany(String shippingCompany) {
+        this.shippingCompany = shippingCompany;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
 }
